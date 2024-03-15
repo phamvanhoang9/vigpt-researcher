dff9482e26b3273e6182d907d6bf38ef1f6ac565
sanslab <sanslab>
2024-03-15 10:21:17 +0700

    functions

---
+++ vigpt_researcher/master/functions.py
@@ -0,0 +1,218 @@
+import sys
+sys.path.append('..')
+
+import asyncio
+from utils.llm import *
+from scraper import Scraper
+from master.prompts import *
+from retrievers import TavilySearch, TavilyNews
+import json
+
+
+def get_retriever(retriever):
+    """ 
+    Gets the retriever
+    Args:
+        retriever: retriever name
+    Returns:
+        retriever: Retriever class
+        
+    """
+    match retriever:
+        case "tavily":
+            retriever = TavilySearch
+        case "tavily_news":
+            retriever = TavilyNews
+        case _:
+            raise Exception("Retriever not found.")
+    
+    return retriever 
+  
+async def choose_agent(query, cfg):
+    """ 
+    Chooses the agent automatically
+    Args:
+        query: original query
+        cfg: Config
+        
+    Returns:
+        agent: Agent name
+        agent_role_prompt: Agent role prompt
+    """
+    try:
+        response = await create_chat_completion(
+            model=cfg.smart_llm_model,
+            messages=[
+                {"role": "system", "content": f"{auto_agent_instructions()}"},
+                {"role": "user", "content": f"task: {query}"}
+            ],
+            temperature=0,
+            llm_provider=cfg.llm_provider
+        )
+        agent_dict = json.loads(response)
+        return agent_dict["server"], agent_dict["agent_role_prompt"]
+    except Exception as e:
+        return "Default Agent", "Bạn là một trợ lý nghiên cứu trí tuệ nhân tạo với vai trò là một nhà tư duy phê bình. Mục đích duy nhất của bạn là viết các báo cáo được đánh giá cao về nội dung được giao một cách khách quan và có cấu trúc."
+    
+async def get_sub_queries(query, agent_role_prompt, cfg):
+    """ 
+    Gets the sub queries 
+    Args:
+        query: original query 
+        agent_role_prompt: agent role prompt 
+        cfg: Config 
+    Returns:
+        sub_queries: List of sub queries
+    """
+    max_research_iterations = cfg.max_iterations if cfg.max_iterations else 1
+    response = await create_chat_completion(
+        model=cfg.smart_llm_model,
+        messages=[
+            {"role": "system", "content": f"{agent_role_prompt}"},
+            {"role": "user", "content": generate_search_queries_prompt(query, max_iterations=max_research_iterations)}
+        ],
+        temperature=0,
+        llm_provider=cfg.llm_provider 
+    )
+    sub_queries = json.loads(response)
+    return sub_queries
+
+def scrape_urls(urls, cfg=None):
+    """ 
+    Scrapes the ulrs
+    Args:
+        urls: List of urls
+        cfg: Config (optional)
+        
+    Returns:
+        text: str
+    """
+    content = []
+    user_agent = cfg.user_agent if cfg else "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
+    try:
+        content = Scraper(urls, user_agent).run()
+    except Exception as e:
+        print(f"{Fore.RED}Error in scrape_urls: {e}{Style.RESET_ALL}")
+    return content
+
+async def summarize(query, content, agent_role_prompt, cfg, websocket=None):
+    """
+    Asynchronously summarizes a list of URLs.
+
+    Args:
+        query (str): The search query.
+        content (list): List of dictionaries with 'url' and 'raw_content'.
+        agent_role_prompt (str): The role prompt for the agent.
+        cfg (object): Configuration object.
+
+    Returns:
+        list: A list of dictionaries with 'url' and 'summary'.
+    """
+    
+    async def handle_task(url, chunk):
+        summary = await summarize_url(query, chunk, agent_role_prompt, cfg)
+        if summary:
+                await stream_output("logs", f"🌐 Tóm tắt url: {url}", websocket)
+                await stream_output("logs", f"📃 {summary}", websocket)
+                
+        return url, summary
+    
+    def chunk_content(raw_content, chunk_size=10000):
+        words = raw_content.split()
+        for i in range(0, len(words), chunk_size):
+            yield ' '.join(words[i:i+chunk_size])
+            
+    concatenated_summaries = []
+    for item in content:
+        url = item['url']
+        raw_content = item['raw_content']
+        
+        # Create tasks for all chunks of the current URL
+        chunk_tasks = [handle_task(url, chunk) for chunk in chunk_content(raw_content)]
+        
+        # Run chunk tasks concurrently
+        chunk_summaries = await asyncio.gather(*chunk_tasks)
+        
+        # Aggregate and concatenate summaries for the current URL
+        summaries = [summary for _, summary in chunk_summaries if summary]
+        concatenated_summary = ' '.join(summaries)
+        concatenated_summaries.append({'url': url, 'summary': concatenated_summary})
+        
+    return concatenated_summaries
+        
+async def summarize_url(query, raw_data, agent_role_prompt, cfg):
+    """
+    Summarizes the text
+    Args:
+        query:
+        raw_data:
+        agent_role_prompt:
+        cfg:
+
+    Returns:
+        summary: str
+        
+    """
+    summary = ""
+    try:
+        summary = await create_chat_completion(
+            model=cfg.fast_llm_model,
+            messages=[
+                {"role": "system", "content": f"{agent_role_prompt}"},
+                {"role": "user", "content": f"{generate_summary_prompt(query, raw_data)}"}
+            ],
+            temperature=0,
+            llm_provider=cfg.llm_provider
+        )
+    except Exception as e:
+        print(f"{Fore.RED}Error in summarizes: {e}{Style.RESET_ALL}")
+    return summary
+
+async def stream_output(type, output, websocket=None, logging=True):
+    """Streams output to the websocket
+    Args:
+        type:
+        output:
+    Returns:
+        None
+    """
+    if not websocket or logging:
+        print(output)
+    
+    if websocket:
+        await websocket.send_json({"type": type, "output": output})
+        
+async def generate_report(query, context, agent_role_prompt, report_type, websocket, cfg):
+    """ 
+    Generates the final report
+    Args:
+        query:
+        context:
+        agent_role_prompt:
+        report_type:
+        websocket:
+        cfg:
+        
+    Returns:
+        report:
+    
+    """
+    generate_prompt = get_report_by_type(report_type)
+    report = ""
+    try:
+        report = await create_chat_completion(
+            model=cfg.smart_llm_model,
+            messages=[
+                {"role": "system", "content": f"{agent_role_prompt}"},
+                {"role": "user", "content": f"{generate_prompt(query, context, cfg.report_format, cfg.total_words)}"}
+            ],
+            temperature=0,
+            llm_provider=cfg.llm_provider,
+            stream=True,
+            websocket=websocket,
+            max_tokens=cfg.smart_token_limit
+        )
+    except Exception as e:
+        print(f"{Fore.RED}Error in generate_report: {e}{Style.RESET_ALL}")
+    
+    return report
