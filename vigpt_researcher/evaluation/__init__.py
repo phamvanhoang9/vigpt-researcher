f6ac609f56bea16880a3d36045f547db1d23fd45
sanslab <sanslab>
2024-03-20 17:18:10 +0700

    metric discription

---
+++ vigpt_researcher/evaluation/__init__.py
@@ -0,0 +1,4 @@
+""" 
+While the LLM models has many metrics to evaluate the performance, I expected evaluating VIGPT Researcher through F1 score !
+I think the F1 score that is the best metric for my project.
+"""
