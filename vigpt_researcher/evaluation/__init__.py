bfaafd7b87eb5b7c68bcbf974e0e727cb2781a52
sanslab <sanslab>
2024-03-21 18:14:36 +0700

    evaluation

--- vigpt_researcher/evaluation/__init__.py
+++ vigpt_researcher/evaluation/__init__.py
@@ -1,4 +1,3 @@
 """ 
-While the LLM models has many metrics to evaluate the performance, I expected evaluating VIGPT Researcher through F1 score !
-I think the F1 score that is the best metric for my project.
+Using BERT Score and Perlexity to evaluate the quality of the generated text.
 """
