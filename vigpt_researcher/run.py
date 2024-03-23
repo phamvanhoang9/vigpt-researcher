8b654834503072a535b251fac2b1cc774e3abd3f
sanslab <sanslab>
2024-03-23 22:38:20 +0700

    RUN VIGPT Researcher

--- vigpt_researcher/run.py
+++ vigpt_researcher/run.py
@@ -3,6 +3,7 @@ sys.path.append('..')
 
 from vigpt_researcher import VIGPTResearcher
 import asyncio 
+import re
 
 
 async def get_report(query: str, report_type: str) -> str:
@@ -12,9 +13,20 @@ async def get_report(query: str, report_type: str) -> str:
     return report
 
 if __name__ == "__main__":
-    query = "Bạn có thể cho tôi biết thông tin về ngành Điện tử - Viễn thông của Đại học Bách khoa Hà Nội được không?"
+    query = "Thông tin về ngành Điện tử - Viễn thông hệ Đại trà của Đại học Bách khoa Hà Nội được không?"
     report_type = "research_report"
     
     report = asyncio.run(get_report(query, report_type))
-    print(report)
     
+    with open("evaluation/hypos.txt", "w", encoding="utf-8") as file:
+        file.write(report) 
+    print("Hypothesis saved to evaluation/hypos.txt")
+        
+    urls = re.findall(r'(https?://\S+)', report)
+    with open("evaluation/refs.txt", "w") as file:
+        for url in urls:
+            file.write(url + '\n')        
+    print("References saved to evaluation/refs.txt")
+    print("\n\n\n")
+    
+    print(report)
