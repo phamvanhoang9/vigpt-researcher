c9ca35211581f5f4e5e55039e366aba6ff366a50
sanslab <sanslab>
2024-03-24 10:12:45 +0700

    colored printing

---
+++ case_study/color_print.py
@@ -0,0 +1,7 @@
+from termcolor import colored
+
+print("-"*80)
+print(colored("Báo cáo đã được lưu vào thư mục evaluation/hypos.txt", "blue"))
+print("-"*80)
+print(colored("Danh sách các tài liệu tham khảo đã được lưu vào thư mục evaluation/refs.txt", "green"))    
+print("-"*80)   
