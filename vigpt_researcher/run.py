b2b4f2cbb595ba7a0020561c5f25791d14db17e8
sanslab <sanslab>
2024-03-24 12:41:21 +0700

    RUN VIGPT Researcher

--- vigpt_researcher/run.py
+++ vigpt_researcher/run.py
@@ -4,6 +4,7 @@ sys.path.append('..')
 from vigpt_researcher import VIGPTResearcher
 import asyncio 
 import re
+from termcolor import colored
 
 
 async def get_report(query: str, report_type: str) -> str:
@@ -12,21 +13,28 @@ async def get_report(query: str, report_type: str) -> str:
     
     return report
 
+def save_report(report, file_path):
+    with open(file_path, "w", encoding="utf-8") as file:
+        file.write(report.strip()) # remove leading and trailing whitespaces
+
 if __name__ == "__main__":
-    query = "Thông tin về ngành Điện tử - Viễn thông hệ Đại trà của Đại học Bách khoa Hà Nội được không?"
+    query = "Cơ hội nghề nghiệp cho sinh viên ngành Điện tử - Viễn thông của Đại học Bách khoa Hà Nội sau khi tốt nghiệp"
     report_type = "research_report"
     
     report = asyncio.run(get_report(query, report_type))
+    report_without_references = report.split("## Tài Liệu Tham Khảo")[0]
     
-    with open("evaluation/hypos.txt", "w", encoding="utf-8") as file:
-        file.write(report) 
-    print("Hypothesis saved to evaluation/hypos.txt")
+    save_report(report_without_references, "evaluation/hypos.txt")
+    print("-"*80)
+    print(colored("Báo cáo đã được lưu vào thư mục evaluation/hypos.txt", "blue"))
+    print("-"*80)
         
     urls = re.findall(r'(https?://\S+)', report)
     with open("evaluation/refs.txt", "w") as file:
         for url in urls:
-            file.write(url + '\n')        
-    print("References saved to evaluation/refs.txt")
-    print("\n\n\n")
+            file.write(url + '\n')    
+    print(colored("Danh sách các tài liệu tham khảo đã được lưu vào thư mục evaluation/refs.txt", "green"))    
+    print("-"*80)
+    print("\n")
     
     print(report)
