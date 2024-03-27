d14d15aacf8a6c31b061aabaf4b5806ec0dff302
sanslab <sanslab>
2024-03-27 11:09:16 +0700

    updating...

--- vigpt_researcher/run.py
+++ vigpt_researcher/run.py
@@ -5,6 +5,7 @@ from vigpt_researcher import VIGPTResearcher
 import asyncio 
 import re
 from termcolor import colored
+import time
 
 
 async def get_report(query: str, report_type: str) -> str:
@@ -18,23 +19,25 @@ def save_report(report, file_path):
         file.write(report.strip()) # remove leading and trailing whitespaces
 
 if __name__ == "__main__":
-    query = "Cơ hội nghề nghiệp cho sinh viên ngành Điện tử - Viễn thông của Đại học Bách khoa Hà Nội sau khi tốt nghiệp"
-    report_type = "research_report"
+    query = "Cơ hội việc làm cho sinh viên ngành Điện tử - Viễn thông của Đại học Bách khoa Hà Nội sau khi tốt nghiệp"
+    report_type = ["báo cáo", "nguồn tham khảo", "khung báo cáo"]
     
-    report = asyncio.run(get_report(query, report_type))
-    report_without_references = report.split("## Tài Liệu Tham Khảo")[0]
+    start_time = time.time()
+    report = asyncio.run(get_report(query, report_type[0]))
+    end_time = time.time()
+    print(colored(f"\nTổng thời gian xử lý: {(end_time - start_time):.3f} giây.\n\n", "cyan"))
     
-    save_report(report_without_references, "evaluation/hypos.txt")
-    print("-"*80)
-    print(colored("Báo cáo đã được lưu vào thư mục evaluation/hypos.txt", "blue"))
-    print("-"*80)
-        
-    urls = re.findall(r'(https?://\S+)', report)
-    with open("evaluation/refs.txt", "w") as file:
-        for url in urls:
-            file.write(url + '\n')    
-    print(colored("Danh sách các tài liệu tham khảo đã được lưu vào thư mục evaluation/refs.txt", "green"))    
-    print("-"*80)
-    print("\n")
+    # report_without_references = report.split("## Tài Liệu Tham Khảo")[0]
+    # save_report(report_without_references, "evaluation/hypos.txt")
+    # print("-"*80)
+    # print(colored("Báo cáo đã được lưu vào thư mục evaluation/hypos.txt", "blue"))
+    # print("-"*80)  
+    # urls = re.findall(r'(https?://\S+)', report)
+    # with open("evaluation/refs.txt", "w") as file:
+    #     for url in urls:
+    #         file.write(url + '\n')    
+    # print(colored("Danh sách các tài liệu tham khảo đã được lưu vào thư mục evaluation/refs.txt", "green"))    
+    # print("-"*80)
+    # print("\n")
     
     print(report)
