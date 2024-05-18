f7b24a0f44c6e3fa78184418d7d13cd98043502b
phamvanhoang9 <hoang.pv192875@sis.hust.edu.vn>
2024-05-18 16:43:22 +0700

    main program

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from backend.server import app
-from dotenv import load_dotenv
+from dotenv import load_dotenv # dotenv module to load environment variables from a .env file into os.environ
 load_dotenv()
 
 if __name__ == "__main__":
     import uvicorn
-
+    """Uvicorn is an ASGI (Asynchronous Server Gateway Interface) server implementation.
+    It is designed to run asynchronous web applications using ASGI, which allows for the building of high-performance asyncio applications.
+    It's particularly well-suited to running FastAPI applications.
+    """
     uvicorn.run(app, host="0.0.0.0", port=8000)
