00527d8fd3c5e41c3f898c86b1ac165b3dd99a60
phamvanhoang9 <hoang.pv192875@sis.hust.edu.vn>
2024-06-17 10:25:44 +0700

    add the similarity threshold

--- vigpt_researcher/context/compression.py
+++ vigpt_researcher/context/compression.py
@@ -15,13 +15,14 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 class ContextCompressor:
     def __init__(self, documents, embeddings, max_results=5, **kwargs):
         self.max_results = max_results
-        self.documents = documents
+        self.documents = documents # documents is a list of Document objects (List[dict[str, Any]]) [{'url': link, 'raw_content': content}, ...]
         self.kwargs = kwargs
         self.embeddings = embeddings
+        self.similarity_threshold = 0.38
 
     def _get_contextual_retriever(self):
         splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-        relevance_filter = EmbeddingsFilter(embeddings=self.embeddings, similarity_threshold=0.78)
+        relevance_filter = EmbeddingsFilter(embeddings=self.embeddings, similarity_threshold=self.similarity_threshold)
         pipeline_compressor = DocumentCompressorPipeline(
             transformers=[splitter, relevance_filter]
         )
