9ec5a1cf6409c31258240ac758e1ceadfd1fbb5b
phamvanhoang9 <hoang.pv192875@sis.hust.edu.vn>
2024-06-17 10:34:37 +0700

    import search platform

--- vigpt_researcher/retrievers/__init__.py
+++ vigpt_researcher/retrievers/__init__.py
@@ -1,7 +1,11 @@
 from .tavily_search.tavily_search import TavilySearch
 from .tavily_news.tavily_news import TavilyNews
+from .duckduckgo.duckduckgo import Duckduckgo
+from .google.google import GoogleSearch 
 
 __all__ = [
     "TavilySearch",
-    "TavilyNews"
+    "TavilyNews",
+    "Duckduckgo",
+    "GoogleSearch"
 ]
